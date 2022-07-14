33d53cc77bf61997b388daf58dcfcc2597f162f4
Matias Piqueras <matiaspiqueras.1994@gmail.com>
2022-07-14 22:17:26 +0200

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,52 +1,58 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+# if status is-interactive
+#     # Commands to run in interactive sessions can go here
+# end
 
-export PATH="$HOME/.cargo/bin:$PATH"
+#export PATH="$HOME/.cargo/bin:$PATH"
+# set LD_LIBRARY_PATH "/opt/homebrew/bin/R"
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
+eval /Users/matiaspiqueras/opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
-#powerline fonts
-set -g theme_powerline_fonts yes
-set -g theme_nerd_fonts no
+#
+# #powerline fonts
+set -g theme_powerline_fonts no
+set -g theme_nerd_fonts yes
+set -g fish_key_bindings fish_vi_key_bindings
+#set -Ux TERM xterm-256color
+# #set kterm
 
 #alias for ll
 if type -q exa
-  alias ll "exa -l -g --icons"
-  alias lla "ll -a"
+ alias ll "exa -l -g --icons"
+ alias lla "ll -a"
 end
 
-#remove greeting
+# #remove greeting
 set fish_greeting
 
-#alias
+#
+#alias ssh="kitty +kitten ssh"
+# #alias
 command -qv nvim && alias vim nvim
 alias cat="bat"
-#setup vpn
+# #setup vpn
 export VPN_HOST="vpn.ku.dk"
 export VPN_USER="bvj349"
 
 function vpn-up
-  sudo openconnect --background --user=$VPN_USER $VPN_HOST
+ sudo openconnect --background --user=$VPN_USER $VPN_HOST
 end
 
 function vpn-down
-  sudo killall openconnect
+ sudo killall openconnect
 end
 
-#connect to sodas server
+# #connect to sodas server
 function sodas -a localhost
-  if test (count -l $argv) -eq 0
-   ssh bvj349@sodascomp01fl.unicph.domain
-  else
-   ssh -L 8855:localhost:8855 bvj349@sodascomp01fl.unicph.domain
-  end
+ if test (count -l $argv) -eq 0
+  ssh bvj349@sodascomp01fl.unicph.domain
+ else
+  ssh -L 8855:localhost:8855 bvj349@sodascomp01fl.unicph.domain
+ end
 end
-
-starship init fish | source
-
+#
 export DBUS_SESSION_BUS_ADDRESS="unix:path=$DBUS_LAUNCHD_SESSION_BUS_SOCKET"
 
+starship init fish | source
